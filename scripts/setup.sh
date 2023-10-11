30b87cd3fd1a873dc365b0e2717017a864c12127
Yuhong Zhong <nanua.zqz@gmail.com>
2023-10-11 12:55:51 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -34,10 +34,6 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-
 php composer.phar install
 
-
 echo 1 | sudo tee /proc/sys/net/ipv4/tcp_tw_reuse
-
-echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo || true
